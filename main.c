93a823360ec513dc52f59a74be40d28b45e5482b
Chris Suran <suran.c@gmail.com>
2020-01-19 19:56:45 -0500

    Get to correct memory addressing

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    static int usage = 1000;
+    static int usage = 1000000;
     int *array = malloc(usage * sizeof(int));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
@@ -11,8 +11,14 @@ int main() {
     else
     {
         for (int i = 0; i < usage; i++) {
-            fprintf(stdout, "usage %d", i);
+            fprintf(stdout, "Appending address to %d byte of allocated memory\n", i);
+            //*(array + sizeof(int)*i) = i;
+            *(array + i) = i;
         }
+
+        static int testaddr = 344;
+        fprintf(stdout, "Contents of address: %d = %d\n", testaddr, array[testaddr]);
+        //fprintf(stdout, "Contents of address: %d = ", testaddr);
 
         sleep(100000);
     }
