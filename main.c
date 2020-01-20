d3bae1b68966b97d98b0bdddcdd5f198a6ebae39
Chris Suran <suran.c@gmail.com>
2020-01-20 00:39:44 -0500

    Switch to two mallocs, stratle resource request limit

--- main.c
+++ main.c
@@ -2,25 +2,35 @@
 #include <stdlib.h>
 
 int main() {
-    static int usage = 620500000;
-    int *array = malloc(usage * sizeof(int));
+    static int usage1 = 620000000;
+    int *array = malloc(usage1 * sizeof(int));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
         return -1;
     }
     else
     {
-        for (int i = 0; i < (usage - 500001); i++) {
+        for (int i = 0; i < usage1; i++) {
             *(array + i) = i*2;
         }
-        fprintf(stdout, "Allocated %u bytes\n", (usage - 500000)*sizeof(int));
+        fprintf(stdout, "Allocated %u bytes\n", usage1*sizeof(int));
         sleep(50);
+    }
 
-        *(array + (usage - 1)) = (usage - 1)*2;
-
-        fprintf(stdout, "Total allocated bytes: %u\n", usage*sizeof(int));
-
-        sleep(100000);
+    static int usage2 = 1000000;
+    fprintf(stdout, "About to allocate %u more bytes\n", usage2*sizeof(int));
+    int *array2 = malloc(usage2 * sizeof(int));
+    if (array2 == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        return -1;
+    }
+    else
+    {
+        for (int i = 0; i < usage2; i++) {
+            *(array2 + i) = i*2;
+        }
+        fprintf(stdout, "Allocated %u bytes\n", usage2*sizeof(int));
+        sleep(50);
     }
     
 }
