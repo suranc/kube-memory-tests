aaff01cb85cb6a64b57e6b5aee9a5a45f0631408
Chris Suran <suran.c@gmail.com>
2020-01-20 00:04:50 -0500

    Move in limit and improve logging

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    static int usage = 620000000;
+    static int usage = 610000000;
     int *array = malloc(usage * sizeof(int));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
@@ -13,12 +13,12 @@ int main() {
         for (int i = 0; i < (usage - 2); i++) {
             *(array + i) = i*2;
         }
-        fprintf(stdout, "Allocated %u bytes\n", (usage - 2));
+        fprintf(stdout, "Allocated %u bytes\n", (usage - 1)*sizeof(int));
         sleep(5);
 
         *(array + (usage - 1)) = (usage - 1)*2;
 
-        fprintf(stdout, "Size of allocated memory: %u\nInteger Count: %u", usage*sizeof(int),usage);
+        fprintf(stdout, "Total allocated bytes: %u\n", usage*sizeof(int));
 
         sleep(100000);
     }
