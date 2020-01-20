3538ed72d0ec597047ecec95bef0a551db469045
Chris Suran <suran.c@gmail.com>
2020-01-20 00:10:59 -0500

    Set limit to 2480M and meter inital allocation to be under that for a few megs, to account for program size

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    static int usage = 610000000;
+    static int usage = 620000000;
     int *array = malloc(usage * sizeof(int));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
@@ -10,10 +10,10 @@ int main() {
     }
     else
     {
-        for (int i = 0; i < (usage - 2); i++) {
+        for (int i = 0; i < (usage - 500001); i++) {
             *(array + i) = i*2;
         }
-        fprintf(stdout, "Allocated %u bytes\n", (usage - 1)*sizeof(int));
+        fprintf(stdout, "Allocated %u bytes\n", (usage - 500000)*sizeof(int));
         sleep(5);
 
         *(array + (usage - 1)) = (usage - 1)*2;
