b5043ecb682251a68f3458d8dc77b6b1eac2ca1d
Chris Suran <suran.c@gmail.com>
2020-01-19 23:51:52 -0500

    Meter allocation and switch to 2800M allocation/request

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    static int usage = 730000000;
+    static int usage = 700000000;
     int *array = malloc(usage * sizeof(int));
     if (array == NULL) {
         fprintf(stderr, "malloc failed\n");
@@ -10,9 +10,13 @@ int main() {
     }
     else
     {
-        for (int i = 0; i < usage; i++) {
+        for (int i = 0; i < (usage - 2); i++) {
             *(array + i) = i*2;
         }
+        fprintf(stdout, "Allocated %u bytes\n", (usage - 2));
+        sleep(5);
+
+        *(array + (usage - 1)) = (usage - 1)*2;
 
         fprintf(stdout, "Size of allocated memory: %u\nInteger Count: %u", usage*sizeof(int),usage);
 
